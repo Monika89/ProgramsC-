3d7f5f07fd419ea36f832bc52a27e84d4ab29177
Monika89 <monika_3ivanova@abv.bg>
2015-05-11 22:34:27 +0300

    test

---
+++ PrintCompany.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class PrintCompany
+{
+    static void Main()
+    {
+        Console.Write("Company name: ");
+        string name = Console.ReadLine();
+        Console.Write("Company address: ");
+        string address = Console.ReadLine();
+        Console.Write("Phone number: ");
+        string phoneNumber = Console.ReadLine();
+        Console.Write("Fax number: ");
+        string faxNumber = Console.ReadLine();
+        if (faxNumber == "")
+        {
+            faxNumber = "(no fax)";
+        }
+        Console.Write("Web site: ");
+        string webSite = Console.ReadLine();
+        Console.Write("Manager first name: ");
+        string managerFirstName = Console.ReadLine();
+        Console.Write("Manager last name: ");
+        string managerLastName = Console.ReadLine();
+        Console.Write("Manager age: ");
+        int managerAge = int.Parse(Console.ReadLine());
+        Console.Write("Manager phone: ");
+        string managerPhone = Console.ReadLine();
+
+        Console.WriteLine("{0}\nAddress: {1}\nTel. {2}\nFax: {3}\nWeb site: {4}\nManager: {5} {6} (age: {7}, tel. {8})", name, address, phoneNumber, faxNumber, webSite, managerFirstName, managerLastName, managerAge, managerPhone);
+    }
+}
